9db551ce3a9558f9940691364f8431a21afc89df
fulfordcs <fulfordcs@gmail.com>
2022-09-14 12:04:31 +0100

    Added switch example

--- Selection/Program.cs
+++ Selection/Program.cs
@@ -10,7 +10,23 @@ namespace Selection
     {
         static void Main(string[] args)
         {
+            Console.Write("What pizza would you like: ");
+            string pizza = Console.ReadLine().ToLower();
 
+            switch(pizza)
+            {
+                case "cheese and tomato":
+                case "margherita":
+                    Console.WriteLine("Suitable for vegetarians");
+                    break;
+                case "meat feast":
+                case "pepperoni":
+                    Console.WriteLine("Not suitable for vegetarians");
+                    break;
+                default:
+                    Console.WriteLine("Unknown pizza!");
+                    break;
+            }
         }
     }
 }
